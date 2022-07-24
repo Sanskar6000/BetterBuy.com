94bcb88d0843e2a354a1233455ffe8acbfae0468
Aayush Mishra <aayushmishra@Aayushs-MacBook-Air.local>
2022-07-24 13:56:12 +0530

    product screen fixed

--- server/data/data.js
+++ server/data/data.js
@@ -1,141 +1,200 @@
 const data = {
-  products: [
-    {
-      id: '1',
-      image:
-        'https://m.media-amazon.com/images/I/41eC3XfDtjL._AC_SR160,160_.jpg',
-      name: "ASIC'S Men's Badminton Shoes",
-      stars: '⭐⭐⭐⭐',
-      rating: '(24,451)',
-      price: '3740.00',
-      slug: 'asics-mens-badminton-shoes',
-      description: 'Nice Product',
-      countInStock: 10,
-      customername: 'vartik',
-      serialnumber: 12345 - 6789,
-      issuetime: '1542',
-      warrantyduration: '6 months',
-      warrantyconditions: 'www.warrantyexample.com',
-    },
-    {
-      id: '2',
-      image:
-        'https://m.media-amazon.com/images/I/41eC3XfDtjL._AC_SR160,160_.jpg',
-      name: "ASIC'S Men's Badminton Shoes",
-      stars: '⭐⭐⭐⭐',
-      rating: '(24,451)',
-      price: '3740.00',
-      slug: 'asics-mens-badminton-shoes',
-      description: 'Nice Product',
-      countInStock: 10,
-      customername: 'vartik',
-      serialnumber: 123456,
-      issuetime: '1542',
-      warrantyduration: '6 months',
-      warrantyconditions: '',
-    },
-    {
-      id: '3',
-      image:
-        'https://m.media-amazon.com/images/I/41eC3XfDtjL._AC_SR160,160_.jpg',
-      name: "ASIC'S Men's Badminton Shoes",
-      stars: '⭐⭐⭐⭐',
-      rating: '(24,451)',
-      price: '3740.00',
-      slug: 'asics-mens-badminton-shoes',
-      description: 'Nice Product',
-      countInStock: 10,
-      customername: 'vartik',
-      serialnumber: 123456,
-      issuetime: '1542',
-      warrantyduration: '6 months',
-      warrantyconditions: 'mmm',
-    },
-    {
-      id: '4',
-      image: '',
-      name: "ASIC'S Men's Badminton Shoes",
-      stars: '⭐⭐⭐⭐',
-      rating: '(24,451)',
-      price: '3740.00',
-      slug: 'asics-mens-badminton-shoes',
-      description: 'Nice Product',
-      countInStock: 10,
-      customername: 'vartik',
-      serialnumber: 123456,
-      issuetime: '1542',
-      warrantyduration: '6 months',
-      warrantyconditions: 'mmm',
-    },
-    {
-      id: '5',
-      image:
-        'https://m.media-amazon.com/images/I/41eC3XfDtjL._AC_SR160,160_.jpg',
-      name: "ASIC'S Men's Badminton Shoes",
-      stars: '⭐⭐⭐⭐',
-      rating: '(24,451)',
-      price: '3740.00',
-      slug: 'asics-mens-badminton-shoes',
-      description: 'Nice Product',
-      countInStock: 10,
-      customername: 'vartik',
-      serialnumber: 123456,
-      issuetime: '1542',
-      warrantyduration: '6 months',
-      warrantyconditions: 'mmm',
-    },
-    {
-      id: '6',
-      image:
-        'https://m.media-amazon.com/images/I/41eC3XfDtjL._AC_SR160,160_.jpg',
-      name: "ASIC'S Men's Badminton Shoes",
-      stars: '⭐⭐⭐⭐',
-      rating: '(24,451)',
-      price: '3740.00',
-      slug: 'asics-mens-badminton-shoes',
-      description: 'Nice Product',
-      countInStock: 10,
-      customername: 'vartik',
-      serialnumber: 123456,
-      issuetime: '1542',
-      warrantyduration: '6 months',
-      warrantyconditions: 'mmm',
-    },
-    {
-      id: '7',
-      image:
-        'https://m.media-amazon.com/images/I/41eC3XfDtjL._AC_SR160,160_.jpg',
-      name: "ASIC'S Men's Badminton Shoes",
-      stars: '⭐⭐⭐⭐',
-      rating: '(24,451)',
-      price: '3740.00',
-      slug: 'asics-mens-badminton-shoes',
-      description: 'Nice Product',
-      countInStock: 10,
-      customername: 'vartik',
-      serialnumber: 123456,
-      issuetime: '1542',
-      warrantyduration: '6 months',
-      warrantyconditions: 'mmm',
-    },
-    {
-      id: '8',
-      image:
-        'https://m.media-amazon.com/images/I/41eC3XfDtjL._AC_SR160,160_.jpg',
-      name: "ASIC'S Men's Badminton Shoes",
-      stars: '⭐⭐⭐⭐',
-      rating: '(24,451)',
-      price: '3740.00',
-      slug: 'asics-mens-badminton-shoes',
-      description: 'Nice Product',
-      countInStock: 10,
-      customername: 'vartik',
-      serialnumber: 123456,
-      issuetime: '1542',
-      warrantyduration: '6 months',
-      warrantyconditions: 'mmm',
-    },
-  ],
-};
-
-module.exports = data;
+  
+  products:[
+   {
+     id: '1',
+     image:
+       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJf5tbZyfdteGy8RdhTP2O1u_7KRh6UwpIfg&usqp=CAU',
+     name: "Kids Shoes",
+     stars: '⭐⭐⭐⭐',
+     rating: '(24,451)',
+     price: '3740.00',
+     slug: 'kids-shoes',
+     description:[
+       'Sole: Polyurethane',
+       'Closure: Lace-Up',
+       'Shoe Width: Medium',
+       'Plaeto Fitsystem: A removable Fitliner under the insole can be peeled off to expand the shoe by up to ½ size. As your feet grow, so does your shoe',
+       'Plaeto365: The proprietary Plaeto365 midsole is responsive, supportive and provides comfort to feet even after a year of usage'
+     ],
+     countInStock: 10,
+     customername:'vartik',
+     serialnumber:'1-234',
+     issuetime:'1234',
+     warrantyduration:'1 year',
+     warrantyconditions:'www.warrantyexample.com'
+   },
+   {
+     id: '2',
+     image: 'https://n4.sdlcdn.com/imgs/i/z/i/8-5-Inch-LCD-Writing-SDL912559138-1-1c558.jpg',
+     name: 'SUPER TOY LCD Writing Tablet',
+     stars: '⭐⭐⭐⭐⭐',
+     rating: '(12,968)',
+     price: '319.00',
+     slug: 'super-toy-lcd-writing-tablet-8.5inch-e-note-pad',
+     description: [
+       'Erase Button: Tablet displays your notes until you erase them with the touch of a button',
+       'Screen Size: 8.5inch. Light Weight, Slim in Size and Easy to Carry',
+       'CARE INSTRUCTION: Please check ON/OFF Erase button present at the back side of the tablet',
+       'Best Birthday Gift For Kids and Adult. USAGE: School, Office, Ruff Work etc',
+       'NOTE: Color may vary as per Availability',
+     ],
+     countInStock: 5,
+     customername:'vartik',
+     serialnumber:'1-244',
+     issuetime:'1234',
+     warrantyduration:'1 year',
+     warrantyconditions:'www.warrantyexample.com'
+   },
+   {
+     id: '3',
+     image:
+       'https://nationaltoday.com/wp-content/uploads/2021/08/Teddy-Bear-Day.jpg',
+     name: 'Teddy Bear Toy',
+     stars: '⭐⭐⭐',
+     rating: '(30)',
+     price: '499.00',
+     slug: 'teddy-bear-toy',
+     description:[
+       'Made from soft, non-toxic and anti-allergic polyester fabric',
+       'Extremely huggable soft toy with conjugate fibre-filled stuffing',
+       'Substantial standing height of 3.5 feet',
+       'Superior quality with strong, secure stitching',
+       'IS-9873 certified to ensure safety',
+     ],
+     countInStock: 7,
+     customername:'vartik',
+     serialnumber:'1-2534',
+     issuetime:'1234',
+     warrantyduration:'1 month',
+     warrantyconditions:'www.warrantyexample.com'
+   },
+ 
+   {
+     id: '4',
+     image:
+       'https://n3.sdlcdn.com/imgs/c/v/i/9788129142146-a2298.jpg',
+     name: 'One Indian Girl',
+     stars: '⭐⭐',
+     rating: '(13,129)',
+     price: '84.00',
+     slug: 'one-indian-girl',
+     description: [
+       'The book begins with Radhika making arrangements regarding her marriage with Brijesh Gulati who works as a software engineer for Facebook in San Francisco.',
+       'She later revealed her childhood and life in Delhi',
+       'It then flashbacks to four years ago, when Radhika began her job training at Goldman Sachs.',
+       'The story flashes back to the present. ',
+       'In the Hong Kong office, she adapts well and even gets a big investment deal in the Philippines',
+     ],
+     countInStock: 8,
+     customername:'vartik',
+     serialnumber:'1-2344',
+     issuetime:'1234',
+     warrantyduration:'1 year',
+     warrantyconditions:'www.warrantyexample.com'
+   },
+   {
+     id: '5',
+     image:
+       'https://target.scene7.com/is/image/Target/GUEST_54f7e884-a145-4601-902f-f8084f619cc5',
+     name: 'Kids School Bag ',
+     stars: '⭐⭐⭐⭐',
+     rating: '(5,713)',
+     price: '298.00',
+     slug: 'dzert-kids-school-bag-soft',
+     description: [
+       'CUTE DESIGN:Made of super soft plush material and high quality smooth zipper accessories. Small backpack suitable for toddler baby kids girl and boy between 1-6 years old.',
+       'SPACIOUS:The toddler backpack presents a main compartment with cotton interior, which is large capacity and long-lasting. Perfectly for keeping and organizing books, toys, pencils and snacks, etc. With the matching pullers and smooth zippers, children could open and close the backpack easily. The front pocket is for keeping the small stuffs easy to reach.',
+       'Material:Non-Toxic And Soft Fabric good quality and Washable Soft And Cuddly Filling, Made By India And Embroidery Bag',
+       'Great Gift:Cute and Cool animal toddler school backpacks, Great Gift for kids with Lovely animal attractive design, Perfect for children going out for travel, preschool and Daycare center.',
+       'With Cotton Interior',
+     ],
+     countInStock: 3,
+     customername:'vartik',
+     serialnumber:'1-234',
+     issuetime:'1234',
+     warrantyduration:'1 year',
+     warrantyconditions:'www.warrantyexample.com'
+   },
+   {
+     id: '6',
+     image:
+       'https://cdn.fcglcdn.com/brainbees/images/products/219x265/10390176a.webp',
+     name: 'Skipping Rope',
+     stars: '⭐⭐⭐⭐⭐',
+     rating: '(9,447)',
+     price: '175.00',
+     slug: 'strauss-skipping-rope-and-jump-rope-for-men-and-women',
+     description: [
+       'Durable & Adjustable Rope',
+       'Jump Fast, Jump Smoothly',
+       'Get Fitness Anytime & Anywhere ',
+       'Portable & Easy to Carry ',
+       'Comfortable Grips, Heavy Jump ropes',
+     ],
+     countInStock: 6,
+     customername:'vartik',
+     serialnumber:'1-2334',
+     issuetime:'1234',
+     warrantyduration:'1 year',
+     warrantyconditions:'www.warrantyexample.com'
+   },
+   {
+     id: '7',
+     image:
+       'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/mens-luxury-watches-1627593848.jpg?crop=0.383xw:0.765xh;0.309xw,0.121xh&resize=640:*',
+     name: 'NOMOS Analog Number Dial',
+     stars: '⭐⭐⭐⭐',
+     rating: '(5,686)',
+     price: '227.00',
+     slug: 'timewear-analog-number-dial-leather-strap',
+     description: [
+       'Wristwatch',
+       'Watch',
+       'Elegant',
+       'Classy',
+       'Men',
+     ],
+     countInStock: 1,
+     customername:'vartik',
+     serialnumber:'1-23754',
+     issuetime:'1234',
+     warrantyduration:'2 year',
+     warrantyconditions:'www.warrantyexample.com'
+   },
+   {
+     id: '8',
+     image: 'https://fs-prod-cdn.nintendo-europe.com/media/images/08_content_images/systems_5/nintendo_switch_3/nintendo_switch_oled/CI_NSwitch_family_hardware_had.png',
+     name: 'Nintendo - Version 2 Switch with Joy-Con',
+     stars: '⭐⭐⭐⭐⭐',
+     rating: '(45,455)',
+     price: '28880.00',
+     slug: 'nintendo-version-2',
+     description: [
+       'Play your way with the Nintendo Switch gaming system',
+       'Whether youre at home or on-the-go, solo or with friends, the Nintendo Switch system is designed to fit your life.',
+       'Dock your Nintendo Switch to enjoy HD gaming on your TV. ',
+       'This is Version 2 Model which will have extra Brightness and Battery life based on the games we play.',
+       'Just undock your console and keep playing in handheld mode',
+     ],
+     countInStock: 5,
+     customername:'vartik',
+     serialnumber:'1-2374',
+     issuetime:'1234',
+     warrantyduration:'1 year',
+     warrantyconditions:'www.warrantyexample.com'
+   },
+ ]
+ };
+ 
+ 
+ 
+ module.exports = data;
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
